e857106efa923829ce86ce49a416d46995fe65a1
bogle <bogleoneil@gmail.com>
2024-04-06 19:49:19 -0400

    Richest Customer Wealth

---
+++ Problems/RichestCustomerWealth.cs
@@ -0,0 +1,118 @@
+﻿using System;
+namespace DataStructureV1.Problems
+{
+    public class RichestCustomerWealth
+    {
+        public RichestCustomerWealth()
+        {
+        }
+
+
+
+        public int RichestCustomerWealthV1(int[,] accounts)
+        {
+
+
+            int[] maxWealth = new int[2];
+
+            int richestCustomer = 0;
+
+
+            for (int i = 0; i < accounts.GetLength(0); i++)
+            {
+
+
+                int highestTotal = 0;
+
+                for (int j = 0; j < accounts.GetLength(1); j++)
+                {
+
+
+                    //Console.WriteLine(accounts[i, j]);
+                    maxWealth[i] += accounts[i, j];
+
+                    highestTotal += accounts[i, j];
+
+                }
+
+                Console.WriteLine(highestTotal);
+
+                if (highestTotal >= richestCustomer)
+                {
+
+                    richestCustomer = highestTotal;
+
+                }
+            }
+
+            var result = string.Join(", ", maxWealth);
+            Console.WriteLine(result);
+
+            return richestCustomer;
+        }
+
+
+        public int RichestCustomerWealthV2(int[][] accounts)
+        {
+            int richestCustomer = 0;
+
+
+            for (int i = 0; i < accounts.Length; i++)
+            {
+
+
+                int highestTotal = 0;
+
+                for (int j = 0; j < accounts[i].Length; j++)
+                {
+
+                    highestTotal += accounts[i][j];
+
+                }
+
+                if (highestTotal >= richestCustomer)
+                {
+
+                    richestCustomer = highestTotal;
+
+                }
+            }
+
+
+            return richestCustomer;
+        }
+
+
+
+        public int RichestCustomerWealthV3(int[][] accounts)
+        {
+            int richestCustomer = 0;
+
+
+            foreach (int[] customer in accounts)
+            {
+
+
+                int highestTotal = 0;
+
+                foreach (int bank in customer)
+                {
+
+                    highestTotal += bank;
+
+                }
+
+                if (highestTotal >= richestCustomer)
+                {
+
+                    richestCustomer = highestTotal;
+
+                }
+            }
+
+
+            return richestCustomer;
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,24 +1,64 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DataStructureV1;
+using DataStructureV1.Problems;
 
 Console.WriteLine("Hello, World!");
 
-
-Console.WriteLine("Running Sum Of 1D Array V1");
-
-var testRunningSumOf1DArray = new RunningSumOf1DArray();
-
-int[] input = new[] { 1, 2, 3, 4 };
-
-var resultv1 = testRunningSumOf1DArray.RunningSumOf1DArrayV1(input);
-
-var resultString = string.Join(", ", resultv1);
-Console.WriteLine(resultString);
+var linebreak = "--------------------";
 
 
-Console.WriteLine("Running Sum Of 1D Array V2");
+//Console.WriteLine("Running Sum Of 1D Array V1");
 
-var resultv2 = testRunningSumOf1DArray.RunningSumOf1DArrayV2(input);
+//var testRunningSumOf1DArray = new RunningSumOf1DArray();
 
-var resultV2String = string.Join(", ", resultv2);
-Console.WriteLine(resultV2String);
+//int[] input = new[] { 1, 2, 3, 4 };
+
+//var resultv1 = testRunningSumOf1DArray.RunningSumOf1DArrayV1(input);
+
+//var resultString = string.Join(", ", resultv1);
+//Console.WriteLine(resultString);
+
+
+//Console.WriteLine("Running Sum Of 1D Array V2");
+
+//var resultv2 = testRunningSumOf1DArray.RunningSumOf1DArrayV2(input);
+
+//var resultV2String = string.Join(", ", resultv2);
+//Console.WriteLine(resultV2String);
+
+
+//Console.WriteLine(linebreak);
+
+
+
+Console.WriteLine("1672. Richest Customer Wealth");
+
+RichestCustomerWealth richestCustomer = new RichestCustomerWealth();
+
+
+var accounts = new[,] { { 1, 2, 3 }, { 3, 2, 1 } };
+
+var maximumWealth = richestCustomer.RichestCustomerWealthV1(accounts);
+
+Console.WriteLine(maximumWealth);
+
+
+Console.WriteLine("1672. Richest Customer Wealth V2");
+
+var accountsV2 = new int[][] {
+    new int[] { 5, 2, 3 },
+    new int[] { 3, 2, 1 },
+    new int[] { 3, 5, 5 },
+
+};
+
+//var maximumWealthV2 = richestCustomer.RichestCustomerWealthV2(accountsV2);
+
+//Console.WriteLine(maximumWealthV2);
+
+var maximumWealthV3 = richestCustomer.RichestCustomerWealthV2(accountsV2);
+Console.WriteLine(maximumWealthV3);
+
+
+Console.WriteLine(linebreak);
+
